499ac453ee10661959675ec0b06d19941a12ae94
AAKKUS1 <aakkus@edenschool.fr>
2025-12-31 16:31:25 +0100

    idk

--- script.js
+++ script.js
@@ -8,3 +8,18 @@ document.addEventListener("click", (e) => {
     });
   }
 });
+
+// const payApi = document.getElementById("payApi");
+// const characterCounter = document.getElementById("characterCounter");
+// const moodLogger = document.getElementById("moodLogger");
+
+// payApi.addEventListener("click", () => {
+//   document.location.href = "https://github.com/AAKKUS1/projet3-pay-api";
+// });
+// characterCounter.addEventListener("click", () => {
+//   document.location.href = "https://github.com/AAKKUS1/AAKKUS1.github.io";
+// });
+// moodLogger.addEventListener("click", () => {
+//   document.location.href =
+//     "https://github.com/AAKKUS1/exam-final-dom-javascript";
+// });
